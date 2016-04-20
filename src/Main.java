9372a2e37d5fab3a0373bf66a93e128780677cef
Poeyan <psoudali@avans.nl>
2016-04-20 15:14:31 +0200

    Verandering

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
+		
+		int[][] Board = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
 	}
 
 }
